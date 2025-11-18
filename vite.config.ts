3ebe1440c55e9010a7b4ffb69ed9d8148fea06e6
Chandrayan Roy <chandrayanroy@gmail.com>
2025-11-18 22:08:41 +0530

    Update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
 			},
 		}),
 		tailwindcss(),
+		checker({ typescript: { tsconfigPath: "tsconfig.app.json" } }),
 	],
 });
